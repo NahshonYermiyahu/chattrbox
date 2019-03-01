1e748dad5a272e53a6a71fbe409f744be672184d
nahshonrm <nahshonrm@gmail.com>
2019-03-01 02:16:01 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let http = require('http');
+let fs = require('fs');
+let extract = require('./extract');
+let wss = require('./websockets-server');
+
+let handleError = function (err, res) {
+    res.writeHead(404);
+    res.end();
+};
+
+let server = http.createServer(function (req, res) {
+    console.log('Responding to a request.');
+     let filePath = extract(req.url);
+     fs.readFile(filePath, function (err, data) {
+         if (err) {
+             handleError(err, res);
+             return;
+         } else {
+             res.end(data);
+         }
+     });
+});
+
+
+server.listen(3000);
---
+++ websockets-server.js
@@ -0,0 +1,27 @@
+let WebSocket = require('ws');
+let WebSocketServer = WebSocket.Server;
+let port = 3001;
+let ws = new WebSocketServer({
+    port:port
+});
+
+let messages =[];
+
+console.log('websockets server started');
+
+ ws.on('connection', function (socket) {
+
+     console.log('client connection established');
+     messages.forEach(function (msg) {
+         socket.send(msg)
+     });
+
+     socket.on('message', function (data) {
+         console.log('message received: ' + data);
+         messages.push(data);
+         ws.clients.forEach(function (clientSocket) {
+             clientSocket.send(data);
+         });
+     });
+ });
+
